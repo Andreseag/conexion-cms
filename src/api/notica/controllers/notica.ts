0ee271b06f1f3c29ffff82530db7633cf398785c
Edwin Castro <edwin.castro@agenciamatch.cl>
2023-10-07 22:21:46 -0500

    feat: add all atributes to get notice by slug :sparkles:

--- src/api/notica/controllers/notica.ts
+++ src/api/notica/controllers/notica.ts
@@ -9,9 +9,11 @@ export default factories.createCoreController('api::notica.notica', ({ strapi })
     const { id } = ctx.params;
 
     const entity = await strapi.db.query('api::notica.notica').findOne({ 
-      where: { slug: id }
+      select: ['title', 'description', 'date', 'author', 'body', 'discharges', 'slug'],
+      where: { slug: id },
+      populate: true
     });
-
+    console.log('entity', entity)
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
     return this.transformResponse(sanitizedEntity);
